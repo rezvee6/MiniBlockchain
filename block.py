f81061d27a6b4d069562d06256e5da8bdef10ca1
Rezvee <rezvee6@gmail.com>
2019-03-12 16:18:24 +0000

    Initialiser added

--- block.py
+++ block.py
@@ -1,2 +1,12 @@
 ## Block structure ##
 
+class Block: 
+    
+    def __init__(self, index, timestamp, date, previousHash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previousHash = previousHash
+        self.hash = self.hashBlock()
+
+    
