921242b0b9b06b24524e10376683c83e30bf41ae
Rezvee <rezvee6@gmail.com>
2019-03-12 16:24:49 +0000

    block creater finished

--- block.py
+++ block.py
@@ -1,12 +1,17 @@
 ## Block structure ##
+import hashlib as hasher
 
 class Block: 
     
     def __init__(self, index, timestamp, date, previousHash):
         self.index = index
         self.timestamp = timestamp
-        self.data = data
+        self.date = date
         self.previousHash = previousHash
         self.hash = self.hashBlock()
 
-    
+    def hashBlock(self):
+        """ Definition of a block, with hashes of its data"""
+        sha = hasher.sha256()
+        sha.update(str(self.index) + str(self.timestamp) + str(self.date) + str(self.previousHash))
+        return sha.hexdigest()
