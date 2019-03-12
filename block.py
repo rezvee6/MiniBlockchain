ba47fc3f2e008d28d394d7bc69c77d5aa2a3cee4
Rezvee <rezvee6@gmail.com>
2019-03-12 16:42:00 +0000

    Initial version complete

--- block.py
+++ block.py
@@ -4,21 +4,42 @@ import datetime as date
 
 class Block: 
     
-    def __init__(self, index, timestamp, date, previousHash):
+    def __init__(self, index, timestamp, data, previousHash):
         self.index = index
         self.timestamp = timestamp
-        self.date = date
+        self.data = data
         self.previousHash = previousHash
         self.hash = self.hashBlock()
 
     def hashBlock(self):
         """ Definition of a block, with hashes of its data"""
         sha = hasher.sha256()
-        sha.update(str(self.index) + str(self.timestamp) + str(self.date) + str(self.previousHash))
+        sha.update(str(self.index) + str(self.timestamp) + str(self.data) + str(self.previousHash))
         return sha.hexdigest()
 
 
 ## Create the genesis block ##
 def createGenesisBlock():
     """ Manually create the genesis block"""
-    return block(0,date.datetime.now(),"First Ever block","0")
+    return Block(0,date.datetime.now(),"First Ever block","0")
+
+## Block creater ##
+def nextBlock(previousBlock):
+    this_index = previousBlock.index + 1
+    this_timestamp = date.datetime.now()
+    this_data = ("Hi!, I'm block"+str(this_index))
+    this_hash = previousBlock.hash
+    return Block(this_index, this_timestamp, this_data, this_hash)
+
+## create the blockchain
+blockchain = [createGenesisBlock()]
+previousBlock = blockchain[0]
+
+## Add arbitary number of blocks
+initBlocks = 10
+for i in range(initBlocks):
+    addBlock = nextBlock(previousBlock)
+    blockchain.append(addBlock)
+    previousBlock = addBlock
+    print("Block #{0} has been added to the blockchain".format(addBlock.index))
+    print("Hash: {0}".format(addBlock.hash))
